0f91386e679c88b67c1cee99b8bbd1921f355de8
Faten Gargouri <gargourifaten3@gmail.com>
2023-10-31 14:50:09 +0100

    Localisation

--- script.js
+++ script.js
@@ -3,3 +3,26 @@ function convertTemperature() {
   var fahrenheit = (celsius * 9/5) + 32;
   document.getElementById("fahrenheit").value = fahrenheit;
 }
+
+const translations = {
+  "Convertisseur de Température": "Temperature Converter",
+  "Celsius:": "Celsius:",
+  "Fahrenheit:": "Fahrenheit:",
+  "Convertir": "Convert"
+};
+
+function changeLanguage() {
+  const selectedLanguage = document.getElementById("language-selector").value;
+
+  document.getElementById("title").textContent = translations["Convertisseur de Température"];
+  document.querySelector('label[for="celsius"]').textContent = translations["Celsius:"];
+  document.querySelector('label[for="fahrenheit"]').textContent = translations["Fahrenheit:"];
+  document.querySelector('button').textContent = translations["Convertir"];
+
+  if (selectedLanguage === "en") {
+    document.getElementById("title").textContent = translations["Convertisseur de Température"];
+    document.querySelector('label[for="celsius"]').textContent = translations["Celsius:"];
+    document.querySelector('label[for="fahrenheit"]').textContent = translations["Fahrenheit:"];
+    document.querySelector('button').textContent = translations["Convertir"];
+  }
+}
